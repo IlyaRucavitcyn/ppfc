c350fc98b47d13ab85b1a956b51d4e79b9387d46
Ilya Rucavitcyn <i.rukavitsin@itransition.com>
2017-04-06 18:02:00 +0300

    Partial workaround with react-router

--- src/components/Payments/Payments.component.js
+++ src/components/Payments/Payments.component.js
@@ -2,17 +2,20 @@ import React, {Component} from 'react';
 import CreditCardForm from '../CreditCardForm/CreditCardForm.component';
 import OtherPayment from '../OtherPayment/OtherPayment.component';
 import RouterPayments from '../RouterPayments/RouterPayments.component';
-import {Router, Route, hashHistory, IndexRoute} from 'react-router';
+import {Router, Route, hashHistory, IndexRoute, useRouterHistory } from 'react-router';
+import { createHashHistory } from 'history'
 import style from './Payments.component.css';
 
 export default class Payments extends Component{
+
   render(){
+    const appHistory = useRouterHistory(createHashHistory)({ queryKey: false });
     return (
       <div className={style.controls}>
           <div className="">
               <h1 className={style.heading}>Payments</h1>
           </div>
-          <Router history={hashHistory}>
+          <Router history={appHistory}>
             <Route path="/" component={RouterPayments}>
               <IndexRoute component={OtherPayment}/>
               <Route path="new" component={CreditCardForm}/>
