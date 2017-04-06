afc1ee6e37ea73dfdae4eb1cfcb75184d1653a15
Ilya Rucavitcyn <i.rukavitsin@itransition.com>
2017-04-06 17:56:58 +0300

    Router started working with extra queries in the url

--- src/components/Payments/Payments.component.js
+++ src/components/Payments/Payments.component.js
@@ -1,6 +1,8 @@
 import React, {Component} from 'react';
 import CreditCardForm from '../CreditCardForm/CreditCardForm.component';
 import OtherPayment from '../OtherPayment/OtherPayment.component';
+import RouterPayments from '../RouterPayments/RouterPayments.component';
+import {Router, Route, hashHistory, IndexRoute} from 'react-router';
 import style from './Payments.component.css';
 
 export default class Payments extends Component{
@@ -10,7 +12,13 @@ export default class Payments extends Component{
           <div className="">
               <h1 className={style.heading}>Payments</h1>
           </div>
-          <CreditCardForm />
+          <Router history={hashHistory}>
+            <Route path="/" component={RouterPayments}>
+              <IndexRoute component={OtherPayment}/>
+              <Route path="new" component={CreditCardForm}/>
+              <Route path="*" component={()=>(<div>Not found...</div>)}/>
+            </Route>
+          </Router>
           <OtherPayment />
       </div>
     );
