ba4843ac29801565a23767552aa513d5aa957817
Ilya Rucavitcyn <i.rukavitsin@itransition.com>
2017-04-27 17:56:32 +0300

    Redux devTools are connected

--- src/index.js
+++ src/index.js
@@ -3,12 +3,16 @@ import {render} from 'react-dom';
 import { Provider } from 'react-redux';
 import paymentForm from "./reducers";
 import PaymentDash from './components/PaymentDash/PaymentDash.component';
-import { applyMiddleware, createStore } from 'redux';
+import { applyMiddleware, createStore, compose } from 'redux';
 import createLogger from 'redux-logger';
 
 const logger = createLogger();
+
+
+
 const store = createStore(
-  paymentForm,
+  paymentForm, /* preloadedState, */
+  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__(),
   applyMiddleware(logger)
 );
 
